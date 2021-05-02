0da23f329bbb588ad84b2bfe4c20db6b48a951a8
Zygimantas <zygimantasaugunas@gmail.com>
2021-05-02 18:56:49 +0300

    FIX: Add missing file.

--- app/src/main/java/com/example/minimum/storage/dao/BookmarkDao.kt
+++ app/src/main/java/com/example/minimum/storage/dao/BookmarkDao.kt
@@ -1,4 +1,25 @@
 package com.example.minimum.storage.dao
 
-class BookmarkDao {
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Query
+import com.example.minimum.storage.model.Bookmark
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface BookmarkDao {
+    @Query("SELECT * FROM bookmark")
+    fun getAll(): List<Bookmark>
+
+    @Query("SELECT * FROM bookmark WHERE article_id = (:articleId)")
+    fun get(articleId: String): Flow<Bookmark?>
+
+    @Query("INSERT INTO bookmark (article_id) VALUES (:articleId)")
+    suspend fun set(articleId: String)
+
+    @Query("DELETE FROM bookmark WHERE article_id = (:articleId)")
+    suspend fun remove(articleId: String)
+
+    @Query("SELECT EXISTS (SELECT 1 FROM bookmark WHERE article_id = :id)")
+    fun exists(id: String): Boolean
 }
